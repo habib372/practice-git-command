dfb2769684a256779056ce502eae34d9cc09ddea
habib372 <habibsoft372@gmail.com>
2022-04-11 20:50:27 +0600

    when we change our code and add

--- index.php
+++ index.php
@@ -9,14 +9,20 @@
 <body>
     <p>Practic  git command</p>
     <?php echo "Hello.. This is just practice command"; ?>
+
     <h3>when you create first repository use all command:</h3><br>
-    <p>$ echo "# practice_git_command" >> README.md~</p>
+    <h3>$ echo "# practice_git_command" >> README.md~</h3>
     <p>git init</p>
-    <p>git commit -m "first commit"</p>
-    <p> git add . </p>       
+    <p>git add .</p>  
+    <p>git commit -m "first commit"</p>     
     <p> git branch -m main </p>       
     <p>$ git remote add origin https://github.com/habib372/your_repository_name.git</p>
     <p>git push -u origin main</p>
+
+    <h3>when you change your repository code:</h3>
+    <p>git add .</p>
+    <p>git commit -m "write your message"</p>
+    <p>git push</p>
     
 </body>
 </html>
