261854e99172086a86790023fd4322344522e3e9
habib372 <habibsoft372@gmail.com>
2022-04-11 20:07:25 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>practic</title>
+</head>
+<body>
+    <p>Practic  git command</p>
+    <?php echo "Hello.. This is just practice command"; ?>
+</body>
+</html>
